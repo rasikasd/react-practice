b934e1e2166efca3146534c3aad5a0bb2ff9d6b5
radh0821 <rasika.dhande@netcracker.com>
2024-03-13 18:02:35 +0530

    assignment 1

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement("div", { id: "parent" },
+    [React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "I am h1"),
+    React.createElement("h2", {}, "I am h2")]),
+    React.createElement("div", { id: "child2" }, [React.createElement("h1", {}, "I am h1 from div 2"),
+    React.createElement("h2", {}, "I am h2 from div 2")])]);  //[] to create multiple elements inside an element
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+console.log(heading);
